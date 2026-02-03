7b328d9a2551d54c9cbe54cbc908acb2acc7298e
primawk <pwkuntoro@gmail.com>
2026-02-03 20:14:22 +0700

    add jwt auth functions with unit test

--- dist/auth.js
+++ dist/auth.js
@@ -1,4 +1,5 @@
 import * as argon2 from "argon2";
+import jwt from "jsonwebtoken";
 export async function hashPassword(password) {
     try {
         return await argon2.hash(password);
@@ -13,5 +14,27 @@ export async function checkPasswordHash(password, hash) {
     }
     catch (err) {
         throw new Error(`Somethins is wrong when comparing the password: ${err}`);
+    }
+}
+export function makeJWT(userId, expiresIn, secret) {
+    const payload = {
+        iss: "chirpy",
+        sub: userId,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000) + expiresIn,
+    };
+    return jwt.sign(payload, secret);
+}
+export function validateJWT(tokenString, secret) {
+    try {
+        const decoded = jwt.verify(tokenString, secret);
+        const userId = decoded.sub?.toString();
+        if (!userId)
+            throw new Error("user does not exist.");
+        return userId;
+    }
+    catch (error) {
+        const errorToken = error;
+        throw new Error(errorToken.message);
     }
 }
---
+++ dist/auth.test.js
@@ -0,0 +1,16 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import { checkPasswordHash, hashPassword } from "./auth";
+describe("Password Hashing", () => {
+    const password1 = "correctPassword123!";
+    const password2 = "anotherPassword456!";
+    let hash1;
+    let hash2;
+    beforeAll(async () => {
+        hash1 = await hashPassword(password1);
+        hash2 = await hashPassword(password2);
+    });
+    it("should return true for the correct password", async () => {
+        const result = await checkPasswordHash(password1, hash1);
+        expect(result).toBe(true);
+    });
+});
